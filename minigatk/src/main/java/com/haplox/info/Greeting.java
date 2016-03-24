46cf94d340c86284f00805556a56eda7a7fc3f55
zhmz90 <zhmz90@gmail.com>
2016-03-24 19:12:08 +0800

    init split

---
+++ minigatk/src/main/java/com/haplox/info/Greeting.java
@@ -0,0 +1,9 @@
+package com.haplox.info;
+
+public class Greeting {
+    public static void sayHi() {
+        System.out.println("Hi!");
+    }
+}
+
+
---
+++ minigatk/src/main/java/com/haplox/info/cmdline/Runner.java
@@ -0,0 +1,10 @@
+package com.haplox.info.cmdline;
+
+import com.haplox.info.Greeting;
+
+public class Runner {
+    public static void main(String args[]) {
+        Greeting.sayHi();
+    }
+}
+
